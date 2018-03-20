2f8fad1b09a7da48b6af6d7609f86011d3c3106e
Burns, Cody <cody.burns@accenture.com>
2018-03-20 11:30:35 -0500

    init
    init

--- public-registry/public-reg.sol
+++ public-registry/public-reg.sol
@@ -11,7 +11,7 @@ pragma solidity ^0.4.11;
 // NOT PRODUCTION READY! DO NOT USE THIS FOR REAL WORLD YET! (or do it, works okay)
 // This managing contract is a general purpose token. Users can register the token address, symbol. decimal, type, and icon location(url for a set fee)
 // It needs an owner who can assign Managers. Managers can edit token details. Output is all meta token data by item.
-// still needs: improved item editing management, test framework, access levels for admins, all outputs for meta data, externalize admin to onlyAdmin contract 
+// still needs: improved item editing management, test framework, access levels for admins, all outputs for meta data, externalize admin to onlyAdmin contract
 // submit pr and issues to https://github.com/realcodywburns/ETC-public-Works/edit/master/public-registry/manager.sol
 
 
@@ -24,8 +24,8 @@ contract owned{
           _;
           }
   }
- 
-  
+
+
 contract priced {
     // Modifiers can receive arguments:
     modifier costs(uint price) {
