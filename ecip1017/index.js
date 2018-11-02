6663821a21edf752841d84bb8a8165e9cf0910a6
Burns, Cody <cody.burns@accenture.com>
2018-11-02 00:41:18 -0500

    add css
    add css
    fix web3 typo

--- ecip1017/index.js
+++ ecip1017/index.js
@@ -1,11 +1,11 @@
-var Web3 = require(web3);
+var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.WEB3_URL));
 //ar web3 = new Web3(new Web3.providers.WebsocketProvider('http://10.0.0.130'));
 
 var http = require('http'),
     fs = require('fs'),
     // NEVER use a Sync function except at start-up!
-    index = fs.readFileSync(__dirname + '/index.html');
+    index = fs.readFileSync(__dirname + 'index.html');
 
 // Send index.html to all requests
 var app = http.createServer(function(req, res) {
